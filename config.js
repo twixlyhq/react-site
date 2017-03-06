d01b7382cec965833d5b2d9c71cd81a3a0e9a1fc
Gabriel Lantz <lantzgabriel@gmail.com>
2017-03-06 22:04:22 +0100

    Committing stuff

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BUCKET_ID: '58b2cbe60b3d4b6e2f97643d',
-  ACCESS_TOKEN: '58b2cbe80b3d4b6e2f976442' // Make sure this is a read only access token
+  BUCKET_ID: '58bcc004f9fe6051996806f0',
+  ACCESS_TOKEN: '58bcc007f9fe6051996806f5' // Make sure this is a read only access token
 };
