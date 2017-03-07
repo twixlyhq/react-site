141ac0edf6f33b6bf8b2812c9d90222a916f10ae
cloudpen <lantzgabriel@gmail.com>
2017-03-07 00:51:12 +0000

    Commiting

--- app/index.js
+++ app/index.js
@@ -3,14 +3,14 @@ import LayoutDefault from './layouts/Default';
 
 var helper = require('../data-store/helper.js');
 
-const App = ({location}) => {
+const App = ({ location }) => {
   const url = location.getUrl();
   
   let data = helper.getPathData(url);
   
   if (!data.current_page) {
-    console.log(url);
-    console.log('NO DATA');
+    // console.log(url);
+    // console.log('NO DATA');
     // return false;
   }
   
