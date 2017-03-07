2ed325b9fb2f470c29825d7098602cdf3754440a
cloudpen <lantzgabriel@gmail.com>
2017-03-07 00:18:39 +0000

    Commiting

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import renderMiddleware from './middleware/render';
 import cacheData from './cache-data.js';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 if (isProduction) {
